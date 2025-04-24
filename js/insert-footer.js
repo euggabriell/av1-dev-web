53aa47756bf366da37360fe39c9b6b27bd3c9245
Patrick Ribeiro Duarte <patrickrd@Laptop-de-Patrick.local>
2025-04-24 19:02:19 -0300

    icon adapt to current theme

--- js/insert-footer.js
+++ js/insert-footer.js
@@ -1,23 +1,32 @@
 import { darkLightModeController } from "./dark-light-mode.js";
 
-const baseUrl = `${window.location.origin}/${window.location.pathname.split("/")[1]}/`
+darkLightModeController()
 
 function insertFooter() {
+    const baseUrl = `${window.location.origin}/${window.location.pathname.split("/")[1]}/`
+
     const footer = document.querySelector("footer")
+    const currentTheme = document.body.getAttribute("data-theme")
 
     footer.innerHTML = `
         <div class="container">
             <p>&copy; 2025 Mercado Econômico - Todos os direitos reservados.</p>
         </div>
 
-        <button id="dark-mode-button">
+        <button id="dark-button" class="theme-mode-button ${currentTheme === "dark" ? "invisible" : ""}">
             <img src="${baseUrl}assets/svg/moon-dark.svg" alt="botão dark/light mode" />
-        </button>
+        </button> 
+
+         <button id="light-button" class="theme-mode-button ${currentTheme === "light" ? "invisible" : ""}">
+            <img src="${baseUrl}assets/svg/sun-light.svg" alt="botão dark/light mode" />
+        </button> 
     `
     
-    document.getElementById("dark-mode-button").addEventListener("click", () => darkLightModeController(true));
+    document.querySelectorAll(".theme-mode-button")
+    .forEach((element) => element.addEventListener("click", () => {
+        darkLightModeController(true)
+        insertFooter()
+    }));
 }
-
-darkLightModeController()
 
 window.addEventListener('load', insertFooter);
